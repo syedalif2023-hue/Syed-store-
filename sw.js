91bca02dc260673a215c09d47c82379e4b0c237b
syedalif2023-hue <syedalif2023@gmail.com>
2026-03-29 16:33:59 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'syed-poultry-v1';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'rates.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
